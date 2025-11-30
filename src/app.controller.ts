2525df7a4d7da484e56b02da3dd5c8324d280d1b
Gökçe Tiryaki <tiryakiigokce@gmail.com>
2025-11-30 19:20:43 +0300

    Enhance AppController with explanatory comments
    Added comments to clarify the purpose of the AppController and its methods.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+//Manin application controller responsible for handling incoming HTTP requests.
 @Controller()
 export class AppController {
+  //Injects AppService to access business logic and response methods.
   constructor(private readonly appService: AppService) {}
 
+  //Handle HTTP GET requests to the root route ('/')
   @Get()
   getHello(): string {
+    //Delegates the response generation to AppService and returns the result.
     return this.appService.getHello();
   }
 }
